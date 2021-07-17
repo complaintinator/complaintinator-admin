3c68700782b28a0745d570bccb516fb90f5bc209
macklark <ganesh.kolavennu@gmail.com>
2021-07-17 11:30:07 +0530

    added some changes

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -1,9 +1,9 @@
 import { useEffect, useState } from "react";
 import { supabase } from "../services/base";
-import moment from "moment";
 import Dashnavbar from "../components/Dashnavbar";
 import Loader from "../components/Loader";
 import Fuse from "fuse.js";
+import Cards from "../components/Cards";
 
 function Admin() {
   const [initData, setData] = useState([]);
@@ -54,18 +54,7 @@ function Admin() {
       {charecterResults.map((instance) => {
         return (
           <section className="mt-5" key={instance.title}>
-            <div className="md:container md:mx-auto flex justify-center">
-              <section className="p-5 w-full md:w-1/2 block bg-gray-700">
-                <p className="text-white text-3xl font-bold cursor-pointer hover:text-blue-400">
-                  {instance.title}
-                </p>
-                {instance.createdOn && (
-                  <p className="mt-2 text-gray-200 font-light text-sm">
-                    {moment(instance.createdOn.toString()).calendar()}
-                  </p>
-                )}
-              </section>
-            </div>
+            <Cards instance={instance} />
           </section>
         );
       })}
