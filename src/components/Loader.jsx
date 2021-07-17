933c747de5e5063d7b50dfde02f4f4b8f5e9d805
macklark <ganesh.kolavennu@gmail.com>
2021-07-17 10:41:33 +0530

    did some changes

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -3,11 +3,11 @@ function Loader() {
     <div className="p-5 w-full md:w-1/2 block bg-gray-700 mb-5">
       <div className="animate-pulse flex space-x-4">
         <div className="rounded-full bg-gray-500 h-12 w-12"></div>
-        <div class="flex-1 space-y-4 py-1">
-          <div class="h-4 bg-gray-500 rounded w-3/4"></div>
-          <div class="space-y-2">
-            <div class="h-4 bg-gray-500 rounded"></div>
-            <div class="h-4 bg-gray-500 rounded w-5/6"></div>
+        <div className="flex-1 space-y-4 py-1">
+          <div className="h-4 bg-gray-500 rounded w-3/4"></div>
+          <div className="space-y-2">
+            <div className="h-4 bg-gray-500 rounded"></div>
+            <div className="h-4 bg-gray-500 rounded w-5/6"></div>
           </div>
         </div>
       </div>
--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -3,9 +3,11 @@ import { supabase } from "../services/base";
 import moment from "moment";
 import Dashnavbar from "../components/Dashnavbar";
 import Loader from "../components/Loader";
+import Fuse from "fuse.js";
 
 function Admin() {
   const [initData, setData] = useState([]);
+  const [initQuery, setQuery] = useState("");
 
   useEffect(() => {
     const loader = async () => {
@@ -19,15 +21,37 @@ function Admin() {
     loader();
   }, []);
 
+  const fuse = new Fuse(initData, {
+    keys: ["id", "title", "email"],
+  });
+
+  const results = fuse.search(initQuery);
+  const charecterResults = initQuery
+    ? results.map((result) => result.item)
+    : initData;
+
+  function searchHandler({ currentTarget = {} }) {
+    const { value } = currentTarget;
+    setQuery(value);
+  }
+
   return (
     <div className="mt-10">
       <Dashnavbar />
+      <div className="mx-auto container mt-10 md:w-1/2 w-full">
+        <input
+          placeholder="🔍 Search..."
+          className="bg-gray-700 p-3 text-white md:w-full w-full focus:outline-none focus:ring focus:border-blue-300 rounded shadow"
+          value={initQuery}
+          onChange={searchHandler}
+        />
+      </div>
       {initData.length === 0 && (
         <div className="md:container md:mx-auto flex justify-center">
           <Loader />
         </div>
       )}
-      {initData.map((instance) => {
+      {charecterResults.map((instance) => {
         return (
           <section className="mt-5" key={instance.title}>
             <div className="md:container md:mx-auto flex justify-center">
